7ddd80341cde51b22d9f644173723687486d9611
leebunce <leebunce@hotmail.com>
2018-09-22 18:12:56 +0100

    Updated pre-poll values

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ evidence <- read_csv("evidence.csv")
 
 # Remove pre-poll duplicates
 donations <- donations %>% 
-  filter(dntn_is_reported_pre_poll %in% c('False', 'Normal'),
+  filter(dntn_is_reported_pre_poll == FALSE,
          level_1 != 'P1') %>% 
   left_join(brexit, by = 'dntn_regulated_entity_name')
 
